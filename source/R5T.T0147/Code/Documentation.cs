e84b8087158f5ac4abccd9e9c66a91a831646585
David Coats <david@getsafetycone.com>
2022-09-21 12:27:17 -0700

    Initial commit.

---
+++ source/R5T.T0147/Code/Documentation.cs
@@ -0,0 +1,12 @@
+using System;
+
+
+namespace R5T.T0147
+{
+	/// <summary>
+	/// IServiceAction<T> library.
+	/// </summary>
+	public static class Documentation
+	{
+	}
+}
---
+++ source/R5T.T0147/Code/Instances.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+namespace R5T.T0147
+{
+    public static class Instances
+    {
+		
+    }
+}
